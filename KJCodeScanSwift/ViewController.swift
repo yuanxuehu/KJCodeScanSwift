35ecbead84fd3891917b4599d158f7fa570f2dd7
TigerHu <yuanxuehu2016@126.com>
2024-08-30 16:48:41 +0800

    Initial Commit

---
+++ KJCodeScanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KJCodeScanSwift
+//
+//  Created by TigerHu on 2024/8/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
